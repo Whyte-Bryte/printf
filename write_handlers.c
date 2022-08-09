9d63cebe1b48164a916c8b91d99eba068298e61c
Funshoalice <107083508+Funshoalice@users.noreply.github.com>
2022-08-09 13:03:24 +0100

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -233,3 +233,21 @@ int write_pointer(char buffer[], int ind, int length,
 			if (extra_c)
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[3], i - 3) + write(1, &buffer[ind], length));
+return (write(1, &buffer[3], i - 3) + write(1, &buffer[ind], length));
+		}
+		else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
+		{
+			if (extra_c)
+				buffer[--padd_start] = extra_c;
+			buffer[1] = '0';
+			buffer[2] = 'x';
+			return (write(1, &buffer[padd_start], i - padd_start) +
+				write(1, &buffer[ind], length - (1 - padd_start) - 2));
+		}
+	}
+	buffer[--ind] = 'x';
+	buffer[--ind] = '0';
+	if (extra_c)
+		buffer[--ind] = extra_c;
+	return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
+}
